68d90155421d24f794663d4611c90c80d03cd4b0
Axolem <Axolemaranjana4@gmail.com>
2023-06-21 23:14:04 +0200

    Make p under header dynamic, add mb; remove mb-6 to mb-5

--- src/components/DashContentWrapper.jsx
+++ src/components/DashContentWrapper.jsx
@@ -1,16 +1,25 @@
-import moment, { months } from "moment/moment";
+import moment from "moment/moment";
 
 const DashContentWrapper = (props) => {
   return (
     <div className="min-h-full px-5" style={{ background: "#F5F5F7" }}>
       <div
         role="header"
-        className="flex justify-content-between align-items-center align-content-center"
+        className="flex justify-content-between align-items-center align-content-center mb-5"
       >
         <div>
-          <h1>{props.routeName}</h1>
+          <h1 className="mb-1">{props.routeName}</h1>
+          <p className="text-gray-500 m-0">
+            {props.text}
+          </p>
         </div>
-        <div>{moment().format("DD MMMM YYYY, HH:MM:SS")}</div>
+        <div className="flex">
+          <i
+            className="pi pi-fw pi-calendar mr-2"
+            style={{ fontSize: "1.2rem" }}
+          />{" "}
+          {moment().format("DD MMMM YYYY, HH:MM:ss")}
+        </div>
       </div>
       {props.children}
     </div>
