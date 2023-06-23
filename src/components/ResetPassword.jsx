2b6107386b9ffbb37527b6f9c99320a1aeddb86a
Axolem <Axolemaranjana4@gmail.com>
2023-06-23 14:53:37 +0200

    Adjust to match the page look p-3 2nd div

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -45,40 +45,42 @@ const ResetPassword = ({ token }) => {
   };
 
   return (
-    <div className="flex flex-column w-20rem mb-7">
-      <Toast ref={toast} />
-      <div className="mb-5">
-        <div className="p-fluid">
-          <div className="p-field">
-            <label htmlFor="password" className="">
-              New Password:
-            </label>
-            <Password
-              id="password"
-              type="password"
-              size="small"
-              className="p-password-input"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-            />
+    <div className="flex flex-column w-20rem mb-6 pl-3">
+      <div className=" pl-3">
+        <Toast ref={toast} />
+        <div className="mb-5">
+          <div className="p-fluid">
+            <div className="p-field">
+              <label htmlFor="password" className="">
+                New Password:
+              </label>
+              <Password
+                id="password"
+                type="password"
+                size="small"
+                className="p-password-input"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="mb-5">
-        <div className="p-fluid">
-          <div className="p-field">
-            <label htmlFor="password" className="">
-              Confirm New Password:
-            </label>
-            <Password
-              id="password"
-              type="password"
-              size="small"
-              className="p-password-input"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              value={confirmPassword}
-            />
+        <div className="mb-5">
+          <div className="p-fluid">
+            <div className="p-field">
+              <label htmlFor="password" className="">
+                Confirm New Password:
+              </label>
+              <Password
+                id="password"
+                type="password"
+                size="small"
+                className="p-password-input"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+              />
+            </div>
           </div>
         </div>
       </div>
