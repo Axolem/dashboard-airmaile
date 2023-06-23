4dbaccddaec008326c5a7dceadcb0f4994b901b5
Axolem <Axolemaranjana4@gmail.com>
2023-06-23 17:52:07 +0200

    Fix: invite component

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Route path="/templates" element={<Templates />} />
       <Route path="/history" element={<Templates />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/invite" element={<Templates />} />
+      <Route path="/invite" element={<Invite />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
