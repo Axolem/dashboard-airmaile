3236f512c1b2dfa86132751f3e244a2620fb65c6
Axolem <Axolemaranjana4@gmail.com>
2023-07-03 03:52:05 +0200

    Fix: Bug not removing dynamic headTags when user leaves the page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,23 +1,28 @@
 import { useEffect } from "react";
 
 const NotFound = () => {
+  
   useEffect(() => {
+    const baseUrl = window.location.origin;
+
+    const head = document.querySelector("head");
+
+    const scriptTag = document.createElement("script");
+    scriptTag.src = `${baseUrl}/404.js`;
+    scriptTag.crossOrigin = "anonymous";
+    head.appendChild(scriptTag);
+
+    document.title = "404 - Not Found";
+
+    const cssTag = document.createElement("link");
+    cssTag.rel = "stylesheet";
+    cssTag.href = `${baseUrl}/404.css`;
+    head.appendChild(cssTag);
+
     return () => {
-      const baseUrl = window.location.origin;
-
-      const head = document.querySelector("head");
-
-      const scriptTag = document.createElement("script");
-      scriptTag.src = `${baseUrl}/404.js`;
-      scriptTag.crossOrigin = "anonymous";
-      head.appendChild(scriptTag);
-
-      document.title = "404 - Not Found";
-
-      const cssTag = document.createElement("link");
-      cssTag.rel = "stylesheet";
-      cssTag.href = `${baseUrl}/404.css`;
-      head.appendChild(cssTag);
+      head.removeChild(scriptTag);
+      head.removeChild(cssTag);
+      document.title = "Airmailer | Dashboard";
     };
   }, []);
 
@@ -27,7 +32,7 @@ const NotFound = () => {
         <h1>404</h1>
         <h2>Uh, Ohh</h2>
         <h3>
-          Sorry we cant find what you are looking for 'cuz its so dark in here
+          Sorry we cant find what you are looking for, cuz its so dark in here
         </h3>
         <a className="no-underline text-center" id="home-link" href="/">
           <span className="text-900 text-2xl no-underline text-center">
