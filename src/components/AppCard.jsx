220b60d7a52f4c884ef64fc306488da59d5749a9
Axolem <Axolemaranjana4@gmail.com>
2023-07-03 04:54:23 +0200

    Add: app slice, actions, state

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -3,13 +3,16 @@ import { Button } from "primereact/button";
 import { ProgressBar } from "primereact/progressbar";
 
 import pickRandomItem from "../helpers/random";
-import { useNavigate } from "react-router";
 
-const AppCard = ({ name = "", id = "non_existance" }) => {
+import { useNavigate } from "react-router";
+import { checkPropTypes } from "prop-types";
+
+const AppCard = ({ name = "", id = "non_existance", data }) => {
   const navigate = useNavigate();
 
   const colorsArr = ["#1F9DE7", "#F9A52B", "#9e0e86", "#57a801", "#5700a1"];
   const color = pickRandomItem(colorsArr);
+
   return (
     <div className="my-shadow bg-white border-round py-3 px-4 m-5  w-18rem h-16rem col-6">
       <div className="flex flex-column align-items-center justify-content-center">
@@ -27,17 +30,18 @@ const AppCard = ({ name = "", id = "non_existance" }) => {
 
       <div>
         <p className="text-muted">
-          Plan: <span>Basic</span>
+          Plan: <span>{data.plan}</span>
         </p>
         <p className="text-muted">
-          Usage: <span>20/5000</span>
+          Usage: <span>{`${data.usage.used}/${data.usage.limit}`}</span>
         </p>
       </div>
       <div className="flex justify-content-between align-items-center">
         <ProgressBar
-          value={20}
+          value={(data.usage.used / data.usage.limit) * 100}
           showValue={true}
           className="h-1rem w-8 mr-1 border-round text-center text-xs"
+          color={color}
         />
         <Button
           label="Manage"
@@ -52,6 +56,12 @@ const AppCard = ({ name = "", id = "non_existance" }) => {
       </div>
     </div>
   );
+};
+
+AppCard.propTypes = {
+  name: checkPropTypes("string", ""),
+  id: checkPropTypes("string", ""),
+  data: checkPropTypes("object", {}),
 };
 
 export default AppCard;
--- src/state/reducers/apps.js
+++ src/state/reducers/apps.js
@@ -1,11 +1,46 @@
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    name: "App 1",
+    plan: "Basic",
+    usage: {
+      used: 0,
+      limit: 5000
+    }
+  },
+  {
+    id: 2,
+    name: "App 2",
+    plan: "Enterprise",
+    usage: {
+      used: 10_000,
+      limit: 500_000
+    }
+  },
+  {
+    id: 3,
+    name: "App 3",
+    plan: "Basic",
+    usage: {
+      used: 300,
+      limit: 5000
+    }
+  }
+];
 
-const todosReducer = (state = initialState, action) => {
+const appsReducer = (state = initialState, action) => {
   switch (action.type) {
     case "GET_TODOS":
       return state;
 
-    case "ADD_TODO":
+    case "ADD_APP":
+      action.payload.todo.id = state.length + 1;
+      action.payload.todo.plan = "Basic";
+      action.payload.todo.usage = {
+        used: 300 * state.length,
+        limit: 5000 * state.length
+      }
+
       return [...state, action.payload.todo];
 
     case "UPDATE_TODO":
@@ -28,4 +63,4 @@ const todosReducer = (state = initialState, action) => {
   }
 };
 
-export default todosReducer;
+export default appsReducer;
