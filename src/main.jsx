249d1064ccb55d0d09de77267fa755eb79ac5a7d
Axolem <Axolemaranjana4@gmail.com>
2023-07-03 02:40:49 +0200

    formatting

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
-import { inject } from '@vercel/analytics';
+import { inject } from "@vercel/analytics";
 import { BrowserRouter } from "react-router-dom";
 
 //theme
@@ -19,14 +19,14 @@ import "primeflex/primeflex.css";
 //custom css
 import "./assets/custom.css";
 
-if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || !import.meta.env.VITE_BASE_URL) {
-  throw new Error("Missing Publishable Key")
+if (
+  !import.meta.env.VITE_CLERK_PUBLISHABLE_KEY ||
+  !import.meta.env.VITE_BASE_URL
+) {
+  throw new Error("Missing Publishable Key");
 }
 
-
- 
 inject();
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
